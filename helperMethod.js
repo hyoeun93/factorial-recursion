eacf91f869621756d4f325a37906e4f43501e748
HyoEun Lee <hyoeun93@gmail.com>
2020-02-19 21:49:43 -0500

    helperMethod

---
+++ helperMethod.js
@@ -0,0 +1,30 @@
+// function outer(input) {
+//     var outerScopedVariable = []
+//     function helper (helperInput) {
+//         helper(helperInput--)
+//     }
+//     helper(input)
+//     return outerScopedVariable
+// }
+
+//Collect all odds num!
+
+function collectOddValues(arr) {
+    let result = [];
+
+    function helper(helperInput) {
+        if(helperInput.length === 0) {
+            return;
+        }
+        if(helperInput[0] % 2 === 0) {
+            result.push(helperInput[0])
+        }
+        helper(helperInput.slice(1))
+    }
+    
+    helper(arr)
+
+    return result;
+}
+
+collectOddValues([1,2,3,4,5,6,7,8,9])
