e1633aaf8ddb22c3ff20f346ddb4e75b0da90afa
HyoEun Lee <hyoeun93@gmail.com>
2020-02-24 21:25:22 -0500

    no swaps

--- bubbleSort.js
+++ bubbleSort.js
@@ -7,14 +7,18 @@ BubbleSort Pseudocode
 */
 
 function bubbleSort(arr) {
-    for(let i = arr.elngth; i > 0 ; i--) {
+    var noSwaps;
+    for(let i = arr.length; i > 0 ; i--) {
+        noSwaps = true;
         for(let j = 0; j < i - 1; j++) {
             if(arr[j] < arr[j+1]) {
                 var temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                noSwaps = false;
             }
         }
+        if(noSwamps) break;
     }
     return arr;
 }
