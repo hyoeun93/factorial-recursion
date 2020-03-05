29e9fa024ac23fb0c21e0943aa027489939a8298
HyoEun Lee <hyoeun93@gmail.com>
2020-03-04 20:59:41 -0500

    mergeSort

---
+++ mergeSort.js
@@ -0,0 +1,33 @@
+function merge(arr1, arr2) {
+    var newArr = [];
+    var i = 0;
+    var j = 0;
+    while(i < arr1.length && j < arr2.length) {
+        if(arr1[i] < arr2[j]) {
+            newArr.push(arr1[i]);
+            i++;
+        } else {
+            newArr.push(arr2[j]);
+            j++;
+        }
+    }
+    while(i < arr1.length) {
+        newArr.push(arr1[i])
+        i++;
+    }
+    while(j < arr2.length) {
+        newArr.push(arr2[j])
+        j++;
+    }
+    return newArr;
+}
+
+function mergeSort(arr) {
+    if(arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
+
+mergeSort([10,24,76,73,72,1,9])
